e1ad821b41ae8ce00a2ddfee8f28b1b10d598285
Juliana Mota <38047358+JulianaMota@users.noreply.github.com>
2018-09-07 00:52:05 +0200

    modal creaction and design

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ function buildCategories(data) {
         const newSection = document.createElement("section");
         const newH2 = document.createElement("h2");
         const newLink = document.createElement("a");
-        newLink.href = "#";
+        newLink.href = "#Menu";
         newLink.textContent = category;
         newLink.addEventListener("click", () => filterBy(category));
 
@@ -67,14 +67,19 @@ function show(plist) {
         const clone = template.cloneNode(true);
 
 
-
+// product list sold out
         if (product.soldout) {
             clone.querySelector(".soldout").classList.remove("hide");
             clone.querySelector(".dishes").classList.add("filter-soldout")
         }
 
+
+// product price
+
         clone.querySelector(".price span").textContent = product.price;
 
+
+// droduct with discount
         if (product.discount) {
             const newPrice = Math.round(product.price - product.price * product.discount / 100);
             clone.querySelector(".price span").textContent = newPrice;
@@ -82,6 +87,8 @@ function show(plist) {
             clone.querySelector(".dishes").classList.add("filter-discount")
         }
 
+
+ //product image
         clone.querySelector(".img-dishes").src = imgbase + "medium/" + product.image + "-md.jpg";
 
         if (product.vegetarian) {
@@ -89,15 +96,27 @@ function show(plist) {
             clone.querySelector(".dishes").classList.add("filter-vegetarian")
         }
 
+// product name
+
         clone.querySelector("h3").textContent = product.name;
+
+
+//procuct description
         clone.querySelector(".description").textContent = product.shortdescription;
+
+// products with alcohol
 
         if (product.alcohol) {
             clone.querySelector(".alcohol span").textContent = product.alcohol;
             clone.querySelector(".alcohol").classList.remove("hide");
             clone.querySelector(".dishes").classList.add("filter-alcohol")
         }
-         clone.querySelector("button.details-button").addEventListener("click", () => fetch(pLink+product.id).then(promise => promise.json()).then(dataProduct =>showDetails(dataProduct)));
+
+//Button for modal
+
+        clone.querySelector("button.details-button").addEventListener("click", () => fetch(pLink + product.id).then(promise => promise.json()).then(dataProduct => showDetails(dataProduct)));
+
+
 
         parent.appendChild(clone);
 
@@ -108,15 +127,73 @@ function show(plist) {
 
 // modal function
 
-function showDetails(product){
+function showDetails(product) {
     console.log(product);
-    modal.querySelector("h2").textContent=product.name;
-    modal.querySelector("p").textContent=product.longdescription;
-    modal.classList.remove("hide");
 
+//image
+    modal.querySelector(".img-details").src = imgbase + "large/" + product.image + ".jpg";
+
+//name
+    modal.querySelector("h2").textContent = product.name;
+
+//category
+    modal.querySelector("h3").textContent = product.category;
+
+//short description
+    modal.querySelector("p.short-des").textContent = product.shortdescription;
+
+//long description
+    modal.querySelector("p.long-des").textContent = product.longdescription;
+
+
+//region of product
+    modal.querySelector("li.region span").textContent = product.region;
+
+//allergens to products
+    if (product.allergens) {
+        modal.querySelector("li.allergens span").textContent = product.allergens;
+
+        modal.querySelector("li.allergens").classList.remove("hide");
+    }
+
+
+// contain alcohol
+    if (product.alcohol) {
+        modal.querySelector("li.alcohol span").textContent = product.alcohol;
+        modal.querySelector("li.alcohol").classList.remove("hide");
+
+    }
+
+
+//price
+    modal.querySelector("li.price span").textContent = product.price;
+
+//vegetation modal
+    if (product.vegetarian) {
+        modal.querySelector(".vegetarian-modal").classList.remove("hide");
+
+    } else {
+        modal.querySelector(".vegetarian-modal").classList.add("hide");
+    }
+
+
+ //sold out modal
+    if (product.soldout) {
+        modal.querySelector(".soldout-modal").classList.remove("hide");
+
+    } else {
+        modal.querySelector(".soldout-modal").classList.add("hide");
+    }
+
+
+
+    modal.classList.remove("hide");
 }
 
-modal.addEventListener("click", ()=>modal.classList.add("hide"));
+
+//close modal
+
+modal.addEventListener("click", () => modal.classList.add("hide"));
 
 
 
